74e92fe26d0a6e59a61c0685a440638718864b38
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-04-09 01:39:09 +0530

    loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,85 +13,97 @@ import Footer from './components/Footer';
 import Product from './Screen/Products/Product';
 import AboutOverview from './Screen/about/AboutOverview';
 import OurVision from './Screen/about/OurVision';
+import React, { useState, useEffect } from 'react';
+import LoadingScreen from './components/LoadingScreen';
+// import scooby from './sc1.png';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/about/overview'
-          element={<AboutOverview />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/about/our-vision'
-          element={<OurVision />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/products'
-          element={<Product />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/blog'
-          element={<Blog />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/faq'
-          element={<Faq />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/contact'
-          element={<Contact />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/services'
-          element={<Service />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/products/castor'
-          element={<CastorOil />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/products/commercial'
-          element={<CommercialGrade />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/products/refined'
-          element={<RefinedGrade />}
-        />
-      </Routes>
-      <Routes>
-        <Route
-          path='/products/coldpressed'
-          element={<ColdPressed />}
-        />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      {loading && <LoadingScreen />}
+      <Router>
+        <Header />
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/about/overview'
+            element={<AboutOverview />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/about/our-vision'
+            element={<OurVision />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/products'
+            element={<Product />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/blog'
+            element={<Blog />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/faq'
+            element={<Faq />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/contact'
+            element={<Contact />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/services'
+            element={<Service />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/products/castor'
+            element={<CastorOil />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/products/commercial'
+            element={<CommercialGrade />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/products/refined'
+            element={<RefinedGrade />}
+          />
+        </Routes>
+        <Routes>
+          <Route
+            path='/products/coldpressed'
+            element={<ColdPressed />}
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../LoadingScreen.css'; // Replace with the actual CSS file for your loading screen
+
+const LoadingScreen = () => {
+  return (
+    <div className='loading-screen'>
+      <img
+        src='https://i.pinimg.com/originals/81/b0/6f/81b06f6ae34692b22aae093a6f34522c.gif'
+        alt=''
+      />
+    </div>
+  );
+};
+
+export default LoadingScreen;
